ee188a5f2e57b04038a4e5b13b726efdebab292b
Gabríela Ólafsdóttir <gabriela16@ru.is>
2020-09-03 10:47:56 +0000

    Initializing and creating a loop that checks for negative integers

--- test_1.py
+++ test_1.py
@@ -1,4 +1,6 @@
-
+# # Create an algorithm that takes in integers until the integer is negative.
+# When the negetive integer in entered the algorithm stops asking for more integers
+# and prints out the highest integer entered and stops.
 
 num_int = int(input("Input a number: "))    # Do not change this line
 
