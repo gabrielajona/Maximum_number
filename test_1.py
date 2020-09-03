244e9c1035e6b4defaf8d78b4d17ff42948f0eab
Gabríela Ólafsdóttir <gabriela16@ru.is>
2020-09-03 10:49:19 +0000

    Creating an if- statement to get the highest integer

--- test_1.py
+++ test_1.py
@@ -3,5 +3,9 @@
 # and prints out the highest integer entered and stops.
 
 num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 0
 
+while num_int >= 0:
+  num_int = int(input("Input a number: "))
+  
 print("The maximum is", max_int)    # Do not change this line
